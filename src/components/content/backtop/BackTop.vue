5206a318d9c958e7b0339f3181b8026f4bb9074c
qdcja <1206026413@qq.com>
2019-11-27 02:22:18 +0800

    首页开发

---
+++ src/components/content/backtop/BackTop.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="back-top">
+    <img src="~assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name:  'BackTop',
+  
+}
+</script>
+
+<style  scoped>
+  .back-top{
+    position: fixed;
+    right: 10px;
+    bottom: 50px;
+  }
+  .back-top img{
+    width: 39px;
+    height: 39px;
+  }
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,154 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <scroll
+      class="content"
+      ref="scroll"
+      :probe-type="3"
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+      <home-recommend-view :recommends="recommends"></home-recommend-view>
+      <feature-view></feature-view>
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        class="tab-control"
+        @tabClick="tabClick"
+      >
+      </tab-control>
+      <goods-list :goods="goods[currentType].list"></goods-list>
+    </scroll>
+
+    <back-top 
+      @click.native="backClick"
+      v-show="isShow">
+    </back-top>
+  </div>
+</template>
+
+<script>
+import HomeSwiper from "./childComponents/HomeSwiper";
+import HomeRecommendView from "./childComponents/HomeRecommendView";
+import FeatureView from "./childComponents/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backtop/BackTop";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+import { log } from "util";
+
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    HomeRecommendView,
+    FeatureView,
+
+    NavBar,
+    TabControl,
+    GoodsList,
+    Scroll,
+    BackTop
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop",
+      isShow:false,
+    };
+  },
+  created() {
+    // 请求多个数据
+    this.getHomeMultidata();
+    // 请求首页商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    // 事件监听相关方法
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0, 500);
+    },
+    contentScroll(position){
+      this.isShow=(-position.y)>1000
+    },
+    loadMore(){
+      this.getHomeGoods(this.currentType)
+    },
+
+    // 网络请求
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list),
+        this.goods[type].page =page,
+        this.$refs.scroll.finishPullUp()
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  background-color: #fff;
+  z-index: 9;
+}
+.content {
+  /* height: 300px; */
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+</style>
